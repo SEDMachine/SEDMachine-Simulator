8572231f3271a20c818088f518fcc0b420d73668
Alex Rudy <alex.rudy@gmail.com>
2011-12-28 10:46:46 +0800

    Adding Distribution System

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "SEDMachineSimulator",
+    version = "0.1.2",
+    py_modules=["SED"],
+    packages = find_packages(
+        exclude=['Tests.*'],
+        ),
+    exclude_package_data = {'': ['.gitignore','bump-version.sh','distribute.sh'], 'Docs/build':['*']},
+    install_requires = ['pyfits>=2.4','numpy>=1.6','scipy>=0.9','matplotlib>=1.1','AstroObject>=0.2.5'],
+    dependency_links = ['https://github.com/alexrudy/AstroObject/tags'],
+    test_suite = 'Tests',
+)
