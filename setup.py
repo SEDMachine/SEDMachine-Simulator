cb31662831ba572f7e03f94f3e9fb9e595136442
Alex Rudy <alex.rudy@gmail.com>
2012-03-28 12:20:45 +0800

    Updated setup.py to use latest github dependency link

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages = ['SEDMachine','SEDSpec2'],
     package_data = {'':['VERSION','README.md','LICENCE'],'SEDspec2':['*.npy','*.dat']},
     version = "0.3.0",
-    install_requires = ['pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','AstroObject>=0.3.0a2'],
-    dependency_links = ['https://github.com/alexrudy/AstroObject/zipball/v0.3.0a1#egg=AstroObject-0.3.0a1'],
+    install_requires = ['pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','AstroObject>=0.3.2'],
+    dependency_links = ['https://github.com/alexrudy/AstroObject/zipball/v0.3.2#egg=AstroObject-0.3.2'],
     test_suite = 'Tests',
     entry_points = {
         'console_scripts' : ['SEDMsim = SEDMachine.Simulator:run', 'SEDMsrc = SEDSpec2.Source:run'],
