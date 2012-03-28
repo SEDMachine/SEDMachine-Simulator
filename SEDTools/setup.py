30cb18f4e4dc5b6879ecad6224b9b5f8f4bada62
Alex Rudy <alex.rudy@gmail.com>
2012-03-28 18:25:40 +0800

    Setup Program Fixes for rename

--- SEDTools/setup.py
+++ SEDTools/setup.py
@@ -1,8 +1,8 @@
 
 def setup():
     """Setup the system"""
-    import SEDSpec2.make_directory
-    import SEDSpec2.make_files
+    import SEDTools.make_directory
+    import SEDTools.make_files
 
 
 if __name__ == "__main__":
