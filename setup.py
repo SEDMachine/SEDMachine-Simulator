a60d63bfc259bbfbf40faec2210d2562270e9da4
Alex Rudy <alex.rudy@gmail.com>
2012-03-29 17:36:26 +0800

    AstroObject dependency update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages = ['SEDMachine','SEDTools'],
     package_data = {'':['VERSION','README.md','LICENCE','*.yaml'],'SEDTools':['Data/*.dat','Data/*.npy']},
     version = "0.3.2",
-    install_requires = ['pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','AstroObject>=0.3.2'],
-    dependency_links = ['https://github.com/alexrudy/AstroObject/zipball/v0.3.2#egg=AstroObject-0.3.2'],
+    install_requires = ['pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','AstroObject>=0.3.3'],
+    dependency_links = ['https://github.com/alexrudy/AstroObject/zipball/v0.3.3#egg=AstroObject-0.3.3'],
     test_suite = 'Tests',
     entry_points = {
         'console_scripts' : ['SEDMsim = SEDMachine.Simulator:run', 'SEDMsetup = SEDTools.setup:run'],
