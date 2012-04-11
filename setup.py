79a4f3d0740035f3a316ce87cf859fcde58a88e5
Alex Rudy <alex.rudy@gmail.com>
2012-04-11 15:34:24 +0800

    setup now installs data modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "SEDMachineSimulator",
-    packages = ['SEDMachine','SEDTools'],
+    packages = ['SEDMachine','SEDTools','SEDTools.Data'],
     package_data = {'':['VERSION','README.md','LICENCE','*.yaml'],'SEDTools':['Data/*.dat','Data/*.npy','*.yaml']},
     version = "0.3.2",
     install_requires = ['pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','AstroObject>=0.3.4'],
