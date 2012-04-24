ff79dce3b144afc620fb1e0c95af1b45d31307a3
Alex Rudy <alex.rudy@gmail.com>
2012-04-24 17:09:37 +0800

    Requiring newer AstroObject for new vocabulary.

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ class SED_build_py(du_build_py):
         
 
 SEDpkgs = find_packages(exclude=['Tests'])
-AstroObjectReq = "0.5-a1"
+AstroObjectReq = "0.5-b1"
 AstroObjectDep = "AstroObject>=" + AstroObjectReq
-AstroObjectVer = "0.5-a1"
+AstroObjectVer = "0.5-b1"
 AstroObjectURL = "https://github.com/alexrudy/AstroObject/zipball/v%(ver)s#egg=AstroObject-%(ver)s" % { 'ver' : AstroObjectVer}
 
 class AstroObjectSourceURL(Command):
