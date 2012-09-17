Fixed subimage size calculation
- Was using max(x)-min(x), but should have been using max(x), as x has had offsets applied.

SEDMachine/Objects.py
@@ -490,8 +490,8 @@ class Lenslet(ImageStack):
         y -= offset[1]
         
         # Get the approximate size of our spectra
-        xdist = np.max(x)-np.min(x)
-        ydist = np.max(y)-np.min(y)
+        xdist = np.max(x)
+        ydist = np.max(y)
         
         # Convert this size into an integer number of pixels for our subimage. This makes it
         # *much* easier to register our sub-image to the master, larger pixel image
@@ -501,8 +501,8 @@ class Lenslet(ImageStack):
         # Create our sub-image, using the x and y width of the spectrum, plus 2 padding widths.
         # Padding is specified in full-size pixels to ensure that the final image is an integer
         # number of full-size pixels across.
-        xsize = xdist+self.config["Instrument"]["padding"]*self.config["Instrument"]["density"]
-        ysize = ydist+self.config["Instrument"]["padding"]*self.config["Instrument"]["density"]
+        xdist += self.config["Instrument"]["padding"]*self.config["Instrument"]["density"]
+        ydist += self.config["Instrument"]["padding"]*self.config["Instrument"]["density"]
         
         # Calculate the resolution inherent to the pixels asked for
         WLS = self.dwl
@@ -526,7 +526,7 @@ class Lenslet(ImageStack):
         self.twl = WLS
         self.tdw = DWL
         self.trs = RS
-        self.subshape = (xsize,ysize)
+        self.subshape = (xdist,ydist)
         self.subcorner = corner
         self.spectrum = spectrum
         self.traced = True
@@ -553,8 +553,8 @@ class Lenslet(ImageStack):
         
         """
         
+
         img = np.zeros(self.subshape)
-        
         for x,y,wl,flux in zip(self.txs,self.tys,self.twl,self.tfl):
             if self.config["Instrument"]["Tel"]["ellipse"]:
                 a = self.fa(wl)